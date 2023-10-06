4a20fa001c3440cd48dc321ab5f2b202a7517d5e
Felipe Ramirez <framirez@farmex.cl>
2023-10-06 18:38:46 -0300

    Configuracion basica de dadbod-ui

--- lua/custom/plugins/dadbod-ui.lua
+++ lua/custom/plugins/dadbod-ui.lua
@@ -3,5 +3,19 @@
 -- Es una interfaz para poder interactuar con dadbod mas agradablemente
 
 return {
-	'kristijanhusak/vim-dadbod-ui'
+	'kristijanhusak/vim-dadbod-ui',
+	dependencies = {
+		{ 'tpope/vim-dadbod',                     lazy = true },
+		{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+	},
+	cmd = {
+		'DBUI',
+		'DBUIToggle',
+		'DBUIAddConnection',
+		'DBUIFindBuffer',
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+	end,
 }
