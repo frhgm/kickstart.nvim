c851109efcec18bffea3ba229919448671c44f70
Felipe Ramirez <framirez@farmex.cl>
2023-10-20 15:49:41 -0300

    Configuracion de OrgMode principalmente lista

--- init.lua
+++ init.lua
@@ -242,6 +242,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
@@ -285,12 +286,15 @@ vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]ea
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'org' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
-  highlight = { enable = true },
+  highlight = {
+    enable                            = true,
+    additional_vim_regex_highlighting = { 'org' },
+  },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
---
+++ lua/custom/plugins/orgmode.lua
@@ -0,0 +1,26 @@
+return
+{ {
+	'nvim-orgmode/orgmode',
+	dependencies = {
+		{ 'nvim-treesitter/nvim-treesitter', lazy = true },
+	},
+	event = 'VeryLazy',
+	config = function()
+		-- Load treesitter grammar for org
+		require('orgmode').setup_ts_grammar()
+
+		-- Setup orgmode
+		require('orgmode').setup({
+			org_agenda_files = '~/orgfiles/**/*',
+			org_default_notes_file = '~/orgfiles/refile.org',
+		})
+	end,
+},
+
+	{
+		'akinsho/org-bullets.nvim',
+		config = function()
+			require('org-bullets').setup()
+		end,
+	}
+}
