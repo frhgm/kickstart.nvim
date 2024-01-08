c0f21e72ee3573e8b3639fd6873cd8562b3f1c2a
Felipe Ramirez <framirez@farmex.cl>
2024-01-08 09:53:00 -0300

    feat: especifico colores de distintos TODOs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,9 +15,10 @@ return
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			keywords = {
+				TODO = { icon = " ", color = "info", alt = { "WIP", "XXX" } },
+				NOTE = { icon = " ", color = "default", alt = { "INFO" } },
+			}
 		}
 	},
 	{
