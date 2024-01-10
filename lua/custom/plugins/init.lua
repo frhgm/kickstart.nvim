c2bdb8a261b376c8d52e8804439540500acc490a
Felipe Ramirez <framirez@farmex.cl>
2024-01-10 10:27:07 -0300

    refactor: saco la mayoria de los plugins de init
    asi ya puedo empezar a tomar el control sobre este!

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,7 +31,7 @@ return
 		"nvim-telescope/telescope-file-browser.nvim",
 		dependencies = { "telescope", "nvim-lua/plenary.nvim" }
 	},
-	{ 'ThePrimeagen/harpoon', name = "harpoon" },
+	{ 'ThePrimeagen/harpoon',  name = "harpoon" },
 	{
 		"folke/twilight.nvim",
 		opts = {
@@ -39,5 +39,119 @@ return
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		}
-	}
+	},
+	{
+		'nvimtools/none-ls.nvim',
+		setup = function(null_ls)
+			require('none-ls').setup {
+				sources = {
+					null_ls.builtins.diagnostics.php
+				}
+			}
+		end
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+	{
+		-- Add indentation guides even on blank lines
+		'lukas-reineke/indent-blankline.nvim',
+		main = 'ibl',
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help indent_blankline.txt`
+		opts = {
+		},
+	},
+	-- Useful plugin to show you pending keybinds.
+	{ 'folke/which-key.nvim', opts = {} },
+	-- Git related plugins
+	'tpope/vim-fugitive',
+	'tpope/vim-rhubarb',
+
+	-- Detect tabstop and shiftwidth automatically
+	'tpope/vim-sleuth',
+	{
+		-- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		},
+	},
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+					{ buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+				vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
+					{ buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+				vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
+					{ buffer = bufnr, desc = '[P]review [H]unk' })
+			end,
+		},
+	},
+	{
+		-- TODO: Mostrar ruta de archivo
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = 'gruvbox',
+				component_separators = '|',
+				section_separators = '',
+			},
+			sections = {
+				lualine_a = {
+					{
+						'filename',
+						path = 1
+					}
+				}
+			}
+		},
+	},
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+	},
+
 };
