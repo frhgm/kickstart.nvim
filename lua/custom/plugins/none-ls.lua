2a741a8972c168b56b0dea8587dd3fb5b7b26f69
Felipe Ramirez <framirez@farmex.cl>
2024-01-29 08:45:26 -0300

    feat: config none-ls

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,14 +1,26 @@
-return {
-	-- 'nvimtools/none-ls.nvim',
-	-- config = function()
-	-- 	local null_ls = require("null-ls")
-	--
-	-- 	null_ls.setup({
-	-- 		sources = {
-	-- 			null_ls.builtins.formatting.stylua,
-	-- 			null_ls.builtins.diagnostics.eslint,
-	-- 			null_ls.builtins.completion.spell,
-	-- 		},
-	-- 	})
-	-- end,
+local M = {
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "mason.nvim" },
+	opts = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
+		local code_actions = null_ls.builtins.code_actions
+		local completion = null_ls.builtins.completion
+		return {
+			sources = {
+				formatting.stylua,
+				formatting.prettier,
+				formatting.eslint,
+				diagnostics.eslint,
+				diagnostics.tsc,
+				code_actions.eslint,
+				code_actions.gitsigns,
+				completion.spell
+			},
+		}
+	end,
 }
+
+return M
