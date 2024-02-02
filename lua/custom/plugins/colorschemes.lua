9a81ad59c1fdb91b626dd0300d3a86ce95dab003
Felipe Ramirez <framirez@farmex.cl>
2024-02-02 17:25:54 -0300

    primeagen's colorschemes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,3 +1,9 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
 	{
 		"craftzdog/solarized-osaka.nvim",
@@ -52,7 +58,33 @@ return {
 		name = 'rose-pine',
 		lazy = true,
 		config = function()
-			vim.cmd.colorscheme 'rose-pine'
+			require('rose-pine').setup({
+				disable_background = true,
+			})
 		end,
-	}
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		lazy = true,
+		config = function()
+			require("tokyonight").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				transparent = true, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = false },
+					keywords = { italic = false },
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			})
+			vim.cmd("colorscheme tokyonight")
+		end
+	},
 };
