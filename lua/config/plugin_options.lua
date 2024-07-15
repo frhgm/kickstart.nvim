87c548d356ef7e36c5325ee4c01b1e31c60e31c1
Felipe Ramírez <framirez@taglermaq.cl>
2024-07-15 13:44:46 -0400

    rama con lazyvim. ya sea de inspiracion o para usarla directamente

---
+++ lua/config/plugin_options.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+}
--- lua/custom/plugins/formatter.lua
+++
@@ -1,7 +0,0 @@
-local config = require("config.formatter")
-return {
-	'mhartington/formatter.nvim',
-	config = function()
-		require('formatter').setup(config)
-	end,
-}
