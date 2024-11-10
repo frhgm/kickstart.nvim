1707963af46fcd57176330913bef26b8f1cab61d
Felipe Ramírez <framirez@taglermaq.cl>
2024-11-10 16:40:17 -0300

    configuro correctamente laravel, reemplazo explorador por oil y dejo deshabilitado lsp_lines

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -229,27 +229,64 @@ return {
   },
   {
     "adalessa/laravel.nvim",
-    ft = "php",
     dependencies = {
-      "nvim-telescope/telescope.nvim",
       "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
-      -- "nvimtools/none-ls.nvim",
+      "kevinhwang91/promise-async",
     },
-    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    cmd = { "Laravel" },
     keys = {
       { "<leader>La", ":Laravel artisan<cr>" },
       { "<leader>Lr", ":Laravel routes<cr>" },
       { "<leader>Lm", ":Laravel related<cr>" },
     },
     event = { "VeryLazy" },
+    opts = {},
     config = true,
-    lazy = true,
   },
   {
-    -- Explorador de directorio como nvim buffer
     "stevearc/oil.nvim",
-    lazy = true,
+    opts = {},
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        view_options = {
+          show_hidden = true,
+        },
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-s>"] = {
+            "actions.select",
+            opts = { vertical = true },
+            desc = "Open the entry in a vertical split",
+          },
+          ["<C-h>"] = {
+            "actions.select",
+            opts = { horizontal = true },
+            desc = "Open the entry in a horizontal split",
+          },
+          ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+          ["<C-p>"] = "actions.preview",
+          ["q"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+          ["g\\"] = "actions.toggle_trash",
+        },
+        float = {
+          padding = 3,
+          border = "rounded",
+        },
+      })
+    end,
   },
   {
     -- Mejor manejo para colapsar o abrir lineas
@@ -263,8 +300,11 @@ return {
   },
   {
     -- Provee una mejor visualización de mensajes de LSP
-    "maan2003/lsp_lines.nvim",
-    lazy = true,
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    enabled = false,
+    config = function()
+      require("lsp_lines").setup()
+    end,
   },
   {
     'SuperBo/fugit2.nvim',
@@ -284,5 +324,9 @@ return {
     keys = {
       { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' }
     }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false
   },
 }
